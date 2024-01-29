f5e56f29e9824ff2c96d9f2c5da5b8f15bef3fc4
landa_lv <62727294+landalv@users.noreply.github.com>
2024-01-29 18:32:21 +0100

    fisrt-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GeneracionesComponent } from './components/generaciones/generaciones.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GeneracionesComponent,
+    PokemonsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
